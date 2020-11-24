3d3785891b290dce13aa7fbb706892f3698dd0ca
Mike Hodges <mikehodges7@outlook.com>
2020-11-23 21:50:25 -0600

    Inital Commit

---
+++ app/src/main/java/com/mhodges/demo3_firebaseauth/MainActivity.java
@@ -0,0 +1,81 @@
+package com.mhodges.demo3_firebaseauth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int RC_SIGN_IN = 0;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                updateUIForUser(user);
+                // ...
+            } else {
+                Toast.makeText(this, "Authentication Failed", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void updateUIForUser(FirebaseUser user) {
+        ((TextView) findViewById(R.id.tvName)).setText(user.getDisplayName());
+        ((TextView) findViewById(R.id.tvEmail)).setText(user.getEmail());
+
+        Uri photoURL = user.getPhotoUrl();
+        if (photoURL != null)
+        {
+            ImageView imageView = (ImageView) findViewById(R.id.ivProfilePicture);
+            Glide.with(this).load(photoURL.toString()).into(imageView);
+        }
+    }
+
+    public void btnSignIn_click(View view) {
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build());
+
+
+        // Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
+    }
+}
